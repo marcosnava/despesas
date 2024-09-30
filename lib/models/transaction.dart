b27daac78f408674e35316167a86137ca53d2cff
marcosnava <nava.marcos@gmail.com>
2024-09-30 15:42:55 -0300

    Setup Inicial

---
+++ lib/models/transaction.dart
@@ -0,0 +1,14 @@
+// models/transaction.dart
+class Transaction {
+  final String id;
+  final String title;
+  final double value;
+  final DateTime date;
+
+  Transaction({
+    required this.id,
+    required this.title,
+    required this.value,
+    required this.date,
+  });
+}
